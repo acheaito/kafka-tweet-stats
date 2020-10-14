d4e4e5a285d61e627e424ef0908bd7ed9f864abe
Ali Cheaito <acheaito@gmail.com>
2020-10-14 14:04:57 -0400

    Eliminate KafkaHelper

--- tweet-consumer/src/main/java/com/cheaito/twitter/TweetConsumerApplication.java
+++ tweet-consumer/src/main/java/com/cheaito/twitter/TweetConsumerApplication.java
@@ -1,16 +1,46 @@
 package com.cheaito.twitter;
 
+import com.cheaito.twitter.domain.Tweet;
+import com.cheaito.twitter.kafka.TopicName;
 import com.cheaito.twitter.kafka.consumer.AnalyticsConsumer;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import javax.enterprise.inject.Produces;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TweetConsumerApplication {
-    public static void main(String[] args) {
+    private final Properties props;
+
+    public static void main(String[] args) throws IOException {
+        new TweetConsumerApplication().start();
+    }
+
+    private void start() {
         Weld weld = new Weld();
         WeldContainer weldContainer = weld.initialize();
         AnalyticsConsumer analyticsConsumer = weldContainer.select(AnalyticsConsumer.class).get();
         Runtime.getRuntime().addShutdownHook(new Thread(analyticsConsumer::shutdown));
         analyticsConsumer.runConsume();
         weld.shutdown();
+    }
+
+    public TweetConsumerApplication() throws IOException {
+        this.props = new Properties();
+        props.load(this.getClass().getClassLoader().getResourceAsStream("kafka.properties"));
+    }
+
+    @Produces
+    @TopicName
+    public String topicName() {
+        return props.getProperty("topic.name");
+    }
+
+    @Produces
+    public Consumer<String, Tweet> consumerInstance() {
+        return new KafkaConsumer<>(props);
     }
 }
--- tweet-producer/src/main/java/com/cheaito/twitter/TweetProducerApplication.java
+++ tweet-producer/src/main/java/com/cheaito/twitter/TweetProducerApplication.java
@@ -1,18 +1,46 @@
 package com.cheaito.twitter;
 
 import com.cheaito.twitter.client.TwitterClient;
+import com.cheaito.twitter.domain.Tweet;
+import com.cheaito.twitter.kafka.TopicName;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import javax.enterprise.inject.Produces;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class TweetProducerApplication {
+    private final Properties props;
+
     public static void main(String[] args) throws IOException, URISyntaxException {
+        new TweetProducerApplication().start();
+    }
+
+    private void start() throws IOException, URISyntaxException {
         Weld weld = new Weld();
         WeldContainer weldContainer = weld.initialize();
         TwitterClient application = weldContainer.select(TwitterClient.class).get();
         application.start();
         weld.shutdown();
+    }
+
+    public TweetProducerApplication() throws IOException {
+        this.props = new Properties();
+        props.load(TweetProducerApplication.class.getClassLoader().getResourceAsStream("kafka.properties"));
+    }
+
+    @Produces
+    @TopicName
+    public String topicName() {
+        return props.getProperty("topic.name");
+    }
+
+    @Produces
+    public Producer<String, Tweet> producerInstance() {
+        return new KafkaProducer<>(props);
     }
 }
