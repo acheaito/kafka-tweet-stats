fac75882d5528f2781972719a8f0f9453772f732
Ali Cheaito <acheaito@gmail.com>
2020-10-19 15:10:54 -0400

    TwitterClient improvements

--- tweet-producer/src/main/java/com/cheaito/twitter/client/TwitterClient.java
+++ tweet-producer/src/main/java/com/cheaito/twitter/client/TwitterClient.java
@@ -3,12 +3,15 @@ package com.cheaito.twitter.client;
 import com.cheaito.twitter.kafka.producer.TweetKafkaProducer;
 import com.cheaito.twitter.model.TwitterApiResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -35,41 +38,47 @@ public class TwitterClient {
     }
 
     public void start() throws URISyntaxException, IOException {
-        URI endpoint = new URI(twitterProps.getProperty("twitter.url")
-                + TWEETS_SAMPLE_STREAM_ENDPOINT
-                + "?tweet.fields=entities,lang,created_at&user.fields=username&place.fields=full_name");
-        long readDelay = Long.parseLong(twitterProps.getProperty("app.readDelay", "1000"));
+        URI endpoint = new URI(twitterProps.getProperty("twitter.url") +
+                TWEETS_SAMPLE_STREAM_ENDPOINT +
+                "?tweet.fields=entities,lang,created_at" +
+                "&user.fields=username" +
+                "&place.fields=full_name");
         Request.Get(endpoint)
                 .addHeader("Authorization", "Bearer " + twitterProps.getProperty("app.bearerToken"))
                 .execute()
-                .handleResponse(httpResponse -> {
-                            if (httpResponse.getStatusLine().getStatusCode() >= HttpStatus.SC_BAD_REQUEST) {
-                                throw new RuntimeException("Bad response from Twitter: "
-                                        + httpResponse.getStatusLine().getStatusCode() + " - " + httpResponse.getStatusLine().getReasonPhrase());
-                            }
-                            BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(httpResponse.getEntity().getContent())));
-                    logger.debug("Starting Twitter API stream loop");
-                            boolean keepReading = true;
-                            while (keepReading) {
-                                String response = br.readLine();
-                                if (response == null) {
-                                    keepReading = false;
-                                }
-                                logger.debug("Parsing api response: {}", response);
-                                if (!response.isEmpty()) {
-                                    TwitterApiResponse apiResponse = objectMapper.readValue(response, TwitterApiResponse.class);
-                                    tweetKafkaProducer.produce(apiResponse.getData());
-                                }
-                                try {
-                                    Thread.sleep(readDelay);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
+                .handleResponse(this::consumeTwitterStream);
+        logger.debug("Done");
+    }
 
-                            }
-                            return null;
-                        }
-                );
-        System.out.println("Exited API consumption call");
+    private Object consumeTwitterStream(HttpResponse httpResponse) throws IOException {
+        if (httpResponse.getStatusLine().getStatusCode() >= HttpStatus.SC_BAD_REQUEST) {
+            throw new RuntimeException("Bad response from Twitter: "
+                    + httpResponse.getStatusLine().getStatusCode() + " - " + httpResponse.getStatusLine().getReasonPhrase());
+        }
+        long readDelay = Long.parseLong(twitterProps.getProperty("app.readDelay", "1000"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(httpResponse.getEntity().getContent())));
+        logger.debug("Starting Twitter API stream loop");
+        boolean keepReading = true;
+        while (keepReading) {
+            String response = br.readLine();
+            if (response == null) {
+                keepReading = false;
+            }
+            logger.debug("Parsing api response: {}", response);
+            try {
+                if (!response.isEmpty()) {
+                    TwitterApiResponse apiResponse = objectMapper.readValue(response, TwitterApiResponse.class);
+                    tweetKafkaProducer.produce(apiResponse.getData());
+                }
+
+                Thread.sleep(readDelay);
+            } catch (Throwable e) {
+                logger.error("Error processing tweets: {}", e.getMessage(), e);
+                keepReading = false;
+            }
+        }
+        logger.debug("Exited Twitter API stream loop");
+        EntityUtils.consume(httpResponse.getEntity());
+        return Response.status(Response.Status.OK).build();
     }
 }
